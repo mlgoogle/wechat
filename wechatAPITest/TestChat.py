fb1eb66cca3365993fb0b2e711d7f9fe4969a64f
BBRick <jiaobiao0505@163.com>
2017-07-25 15:35:07 +0800

    修改命令行二维码

--- wechatAPITest/TestChat.py
+++ wechatAPITest/TestChat.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
-# coding=utf-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#encoding=utf-8
 
 from core import Core
 
@@ -7,24 +8,22 @@ cr = Core()
 
 @cr.msg_register('Text')
 def receiveMsg(msg):
-    group = cr.search_chatrooms(name='Happy')[0]
-    name = group['UserName']
-    cr.send('%s : %s' % ( msg['User']['NickName'], msg['Text']), toUserName=name)
 
+    sendMsgToGroup('%s : %s' % ( msg['User']['NickName'], msg['Text']))
 
 @cr.msg_register('Text', isGroupChat=True)
 def receiveGrope(msg):
-    group = cr.search_chatrooms(name='Happy')[0]
-    name = group['UserName']
-    cr.send('%s : %s' % ( msg['User']['NickName'], msg['Text']), toUserName=name)
+    sendMsgToGroup('%s : %s' % ( msg['User']['NickName'], msg['Text']))
+
 
 def sendMsgToGroup(msg):
     group = cr.search_chatrooms(name='Happy')[0]
     name = group['UserName']
     cr.send('%s' % msg, toUserName=name)
-cr.auto_login()
-sendMsgToGroup('robot has been login.')
+cr.auto_login(enableCmdQR=True)
 
+
+sendMsgToGroup('robot has been login.')
 cr.run()
 
 
