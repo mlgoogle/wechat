3d515e32e27f6b3377dc4708a9af83bd6fda03aa
BBRick <jiaobiao0505@163.com>
2017-07-27 17:51:16 +0800

    测试事件

--- Event/EventTest.py
+++ Event/EventTest.py
@@ -3,13 +3,15 @@ import os
 import threading
 
 
-def initLibEvent(self):
+def initLibEvent():
 
    return libevent.Base()
 
 def creatEvent():
     event = threading.Event()
     ev = libevent.Event(initLibEvent(), 'fd', libevent.EV_READ|libevent.EV_PERSIST, recall, event)
+    ev.add(10)
+    ev.loop()
 
 def recall(ev, fd, what, event):
     print fd
